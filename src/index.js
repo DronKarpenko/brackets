7a8ec1c9d86f3a18b7c542045261121126f3991b
Andrey <dron.donbass@gmail.com>
2021-03-08 11:46:13 +0200

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let closeBrackets =[]; 
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  } 
+  
+  let toClose = [];
+  
+  for (let i = 0; i < str.length; i++) { 
+    if(closeBrackets.indexOf(str[i]) !== -1 && toClose.length) {		
+      let lastNotClosed = toClose[toClose.length - 1];
+      const openedBracketIndex = openBrackets.indexOf(lastNotClosed);
+      if(closeBrackets[openedBracketIndex] === str[i]) {
+        toClose.pop();
+        continue;
+      }
+    }
+    if(openBrackets.indexOf(str[i]) !== -1) {
+    	toClose.push(str[i]);
+    }
+    // Если нет, то вернуть false
+    else {
+    return false;
+    }
+  }
+  if(toClose.length > 0) {
+    return false;
+  }
+  else {
+  return true;
+  }
 }
